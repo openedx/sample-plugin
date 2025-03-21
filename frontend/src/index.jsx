2daf37ffb8a34823e61908e8ef9bd0d1b89c295b
Feanil Patel <feanil@axim.org>
2025-03-21 13:59:34 -0400

    Add an index to expose the plugin.

---
+++ frontend/src/index.jsx
@@ -0,0 +1,4 @@
+import customCourseListPlugin from './plugin'
+
+/* If we want to add more plugins, we would import them above and then add them to the list of exports below. */
+export {customCourseListPlugin}
