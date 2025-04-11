0ac9e07f7cc986f966d170bf8305487a7f7eea93
Feanil Patel <feanil@axim.org>
2025-04-11 10:11:54 -0400

    feat: Add backend plugin from the cookiecutter.

---
+++ backend/sample_plugin/apps.py
@@ -0,0 +1,13 @@
+"""
+sample_plugin Django application initialization.
+"""
+
+from django.apps import AppConfig
+
+
+class SamplePluginConfig(AppConfig):
+    """
+    Configuration for the sample_plugin Django application.
+    """
+
+    name = 'sample_plugin'
